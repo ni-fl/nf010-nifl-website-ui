fbc39cea514a4f5bf53d45383bbb5cff10c4035e
jeromehaas <hello@jeromehaas.ch>
2025-07-12 17:23:29 +0200

    Refactor: pm2 config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,21 @@
+// ecosystem.config.cjs
 module.exports = {
   apps: [
     {
       name: "my-remix-app",
-      cwd: "/home/deployer/my-remix-app",
-      script: "node",
-      args: "build/index.js",
-      autorestart: true,
-      env: {
+      cwd: "/home/deployer/Development/projects/nf010-nifl-website-ui",
+
+      // Use npm start so it runs "node build/index.js"
+      script: "npm",
+      args: "start",
+
+      env_production: {
         NODE_ENV: "production",
         PORT: 3000
       },
+
+      autorestart: true
     }
   ]
 };
+;
